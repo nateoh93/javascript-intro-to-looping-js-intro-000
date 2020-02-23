77df177686e56ff57fa5e186fe2063a278e9d479
Nathan Oh <nathanoh93@gmail.com>
2020-02-23 21:22:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,3 +3,5 @@ function forLoop(array) {
   console.log(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`)
   }
 }
+const strangeArray = forLoop (array)
+const testArray = strangeArray.slice(array.length)
