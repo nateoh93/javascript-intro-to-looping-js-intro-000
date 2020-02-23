ddde263509cd76814f3d6c88e0514c7efd761b13
Nathan Oh <nathanoh93@gmail.com>
2020-02-23 20:36:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (var array=0; array<26, array++) {
+  for (var array=0; array<26; array++) {
   console.log(`"I am ${array} strange loop${array===0? "": 's'}."`)
   }
 }
